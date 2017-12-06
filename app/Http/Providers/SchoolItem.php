01cca6706ec90ab7bff50607d7be87a7e151aad8
chiawen72003 <444>
2017-12-06 14:37:45 +0800

    內容更新

--- app/Http/Providers/SchoolItem.php
+++ app/Http/Providers/SchoolItem.php
@@ -2,7 +2,8 @@
 
 namespace App\Http\Providers;
 
-use App\Http\Models\NewsList;
+use App\Http\Models\School;
+use App\Http\Models\SchoolSubject;
 use Illuminate\Support\Str;
 use \Input;
 
@@ -30,190 +31,115 @@ class SchoolItem
     }
 
     /**
-     * 取得 所有的系統公告列表
-     *
-     * @return Array $list_data 系統公告資料
-     */
-    public function get_all_news()
-    {
-        $list_data = array();
-        $temp_obj = NewsList::orderBy('id', 'DESC')
-            ->get();
-        foreach ($temp_obj as $value) {
-            $list_data[] = $value;
-        }
-
-        return $list_data;
-    }
-
-    /**
-     * 取得 系統公告列表
-     *
-     * @return Array $list_data 系統公告資料
-     */
-    public function get_news_list()
-    {
-        $list_data = array();
-        $temp_obj = NewsList::orderBy('id', 'DESC')
-            ->paginate(20);
-
-        return $temp_obj;
-    }
-
-    /**
-     * 取得 一筆系統公告資料
-     *
-     * @return Array $news_data 系統公告資料
-     */
-    public function get_old_data()
-    {
-        $news_data = null;
-        $temp_obj = NewsList::where('id', $this->input_array['id'])
-            ->get();
-        foreach ($temp_obj as $value) {
-            $news_data = $value;
-        }
-
-        return $news_data;
-    }
-
-    /**
-     * 新增 系統資料
+     * 新增 科系資料
      *
      */
-    public function add_data()
+    public function add_subject()
     {
-        if ($this->input_array['title']) {
-            $update = new NewsList();
-            $update->title = $this->input_array['title'];
-            $update->dsc = $this->input_array['dsc'];
-            $update->save();
-            $getID = $update->id;
-
-            //處理上傳圖片檔案
-            $files = Input::file('updateFile');
-            if (count($files) > 0 and $files != null) {
-                $lists = $this->uploadFile($files, $getID);
-            }
-        }
-
-        return;
-    }
-
-
-    /**
-     * 更新 系統資料
-     *
-     */
-    public function update_data()
-    {
-        if ($this->input_array['id'] AND $this->input_array['title']) {
-            $update = NewsList::find($this->input_array['id']);
-            $update->title = $this->input_array['title'];
-            $update->dsc = $this->input_array['dsc'];
-            $update->save();
-            $getID = $this->input_array['id'];
-
-            //處理上傳圖片檔案
-            $files = Input::file('updateFile');
-            if (count($files) > 0 and $files != null) {
-                if ($update->file_path > '' AND file_exists($update->file_path)) {
-                    unlink('./' . $update->file_path);
-                }
-                $lists = $this->uploadFile($files, $getID);
-            }
-        }
-
-        return;
-    }
-
-    /**
-     * 移除一筆系統資料
-     *
-     */
-    public function delete_data()
-    {
-        if ($this->input_array['id']) {
-            $temp_data = NewsList::where('id', $this->input_array['id'])->get();
-            foreach ($temp_data as $v) {
-                if ($v['file_path'] > '' AND file_exists('upfire/file' . $v['file_path'])) {
-                    unlink('upfire/file' . $v['file_path']);
-                }
-            }
-            NewsList::destroy($this->input_array['id']);
-        }
-
-        return;
-    }
-
-
-    //上傳圖片檔
-    protected static function uploadFile($file, $id)
-    {
-        $upload_path = env('NEWS_FILE_BATH');
-        $file_name = Str::random(32);
-        $org_name = $file->getClientOriginalName();
-
-        $mime = $file->getMimeType();
-        $ext = '';
-        switch ($mime) {
-            case 'image/png':
-                $ext = 'png';
-                break;
-            case 'image/pjpeg':
-            case 'image/jpg':
-            case 'image/jpeg':
-                $ext = 'jpg';
-                break;
-            case 'image/gif':
-                $ext = 'gif';
-                break;
-            case 'image/x-ms-bmp':
-                $ext = 'bmp';
-                break;
-            default:
-                $_t = explode('.', $org_name);
-                if (count($_t) > 0) {
-                    $ext = $_t[count($_t) - 1];
-                }
-                break;
-        }
-        if (!empty($ext)) {
-            $file_name = "{$file_name}.{$ext}";
-        }
-
-
-        $real_path = public_path() . DIRECTORY_SEPARATOR . $upload_path;
-
-        $year = date('Y');
-        $month = date('m');
-        $day = date('d');
-        if (!file_exists($real_path)) {
-            mkdir($real_path);
-        }
-        $real_path = $real_path . DIRECTORY_SEPARATOR . $year;
-        if (!file_exists($real_path)) {
-            mkdir($real_path);
-        }
-        $real_path = $real_path . DIRECTORY_SEPARATOR . $month;
-        if (!file_exists($real_path)) {
-            mkdir($real_path);
-        }
-        $real_path = $real_path . DIRECTORY_SEPARATOR . $day;
-        if (!file_exists($real_path)) {
-            mkdir($real_path);
-        }
-
-        $destinationPath = $real_path;
-        $file->move($destinationPath, $file_name);
-
-        $file_path = $upload_path . '/' . $year . '/' . $month . '/' . $day . '/' . $file_name;
-
-        $update = NewsList::find($id);
-        $update->file_path = $file_path;
-        $update->file_name = $org_name;
+        $update = new SchoolSubject();
+        $update->school_id = $this->input_array['school_id'];
+        $update->subject_title = $this->input_array['subject_title'];
         $update->save();
+        $this->msg = array(
+            'status' => true,
+            'msg' => '新增成功!',
+        );
 
-        return $update;
+        return $this->msg;
     }
+
+
+    /**
+     * 更新 科系資料
+     *
+     */
+    public function update_subject()
+    {
+        if (isset($this->input_array['id']) && isset($this->input_array['subject_title'])) {
+            $update = SchoolSubject::find($this->input_array['id']);
+            $update->subject_title = $this->input_array['subject_title'];
+            $update->save();
+            $this->msg = array(
+                'status' => true,
+                'msg' => '更新成功!',
+            );
+        }
+
+        return $this->msg;
+    }
+
+    /**
+     * 移除 科系資料
+     *
+     */
+    public function delete_subject()
+    {
+        if (isset($this->input_array['id'])) {
+            SchoolSubject::destroy($this->input_array['id']);
+            $this->msg = array(
+                'status' => true,
+                'msg' => '刪除成功!',
+            );
+        }
+
+        return $this->msg;
+    }
+
+
+    /**
+     * 新增 學校資料
+     *
+     */
+    public function add_school()
+    {
+        $update = new School();
+        $update->city = $this->input_array['city'];
+        $update->code = $this->input_array['code'];
+        $update->school_title = $this->input_array['school_title'];
+        $update->save();
+        $this->msg = array(
+            'status' => true,
+            'msg' => '新增成功!',
+        );
+
+        return $this->msg;
+    }
+
+
+    /**
+     * 更新 科系資料
+     *
+     */
+    public function update_school()
+    {
+        if (isset($this->input_array['id']) && isset($this->input_array['school_title'])) {
+            $update = School::find($this->input_array['id']);
+            $update->school_title = $this->input_array['school_title'];
+            $update->save();
+            $this->msg = array(
+                'status' => true,
+                'msg' => '更新成功!',
+            );
+        }
+
+        return $this->msg;
+    }
+
+    /**
+     * 移除 科系資料
+     *
+     */
+    public function delete_school()
+    {
+        if (isset($this->input_array['id'])) {
+            School::destroy($this->input_array['id']);
+            $this->msg = array(
+                'status' => true,
+                'msg' => '刪除成功!',
+            );
+        }
+
+        return $this->msg;
+    }
+
 }
--- app/Http/Providers/StructureItem.php
+++ app/Http/Providers/StructureItem.php
@@ -2,7 +2,8 @@
 
 namespace App\Http\Providers;
 
-use App\Http\Models\News;
+use App\Http\Models\UnitStructure;
+use App\Http\Models\Course;
 use Illuminate\Support\Str;
 use \Input;
 
@@ -21,6 +22,10 @@ class StructureItem
         'file' => null,
         'updateFile' => null,
     );
+    private $msg = array(
+        'status' => false,
+        'msg' => '',
+    );
 
     public function init($input_data = array())
     {
@@ -30,190 +35,117 @@ class StructureItem
     }
 
     /**
-     * 取得 所有的系統公告列表
-     *
-     * @return Array $list_data 系統公告資料
-     */
-    public function get_all_news()
-    {
-        $list_data = array();
-        $temp_obj = NewsList::orderBy('id', 'DESC')
-            ->get();
-        foreach ($temp_obj as $value) {
-            $list_data[] = $value;
-        }
-
-        return $list_data;
-    }
-
-    /**
-     * 取得 系統公告列表
-     *
-     * @return Array $list_data 系統公告資料
-     */
-    public function get_news_list()
-    {
-        $list_data = array();
-        $temp_obj = NewsList::orderBy('id', 'DESC')
-            ->paginate(20);
-
-        return $temp_obj;
-    }
-
-    /**
-     * 取得 一筆系統公告資料
-     *
-     * @return Array $news_data 系統公告資料
-     */
-    public function get_old_data()
-    {
-        $news_data = null;
-        $temp_obj = NewsList::where('id', $this->input_array['id'])
-            ->get();
-        foreach ($temp_obj as $value) {
-            $news_data = $value;
-        }
-
-        return $news_data;
-    }
-
-    /**
-     * 新增 系統資料
+     * 新增 單元結構資料
      *
      */
-    public function add_data()
+    public function add_unit()
     {
-        if ($this->input_array['title']) {
-            $update = new NewsList();
-            $update->title = $this->input_array['title'];
-            $update->dsc = $this->input_array['dsc'];
-            $update->save();
-            $getID = $update->id;
-
-            //處理上傳圖片檔案
-            $files = Input::file('updateFile');
-            if (count($files) > 0 and $files != null) {
-                $lists = $this->uploadFile($files, $getID);
-            }
-        }
-
-        return;
-    }
-
-
-    /**
-     * 更新 系統資料
-     *
-     */
-    public function update_data()
-    {
-        if ($this->input_array['id'] AND $this->input_array['title']) {
-            $update = NewsList::find($this->input_array['id']);
-            $update->title = $this->input_array['title'];
-            $update->dsc = $this->input_array['dsc'];
-            $update->save();
-            $getID = $this->input_array['id'];
-
-            //處理上傳圖片檔案
-            $files = Input::file('updateFile');
-            if (count($files) > 0 and $files != null) {
-                if ($update->file_path > '' AND file_exists($update->file_path)) {
-                    unlink('./' . $update->file_path);
-                }
-                $lists = $this->uploadFile($files, $getID);
-            }
-        }
-
-        return;
-    }
-
-    /**
-     * 移除一筆系統資料
-     *
-     */
-    public function delete_data()
-    {
-        if ($this->input_array['id']) {
-            $temp_data = NewsList::where('id', $this->input_array['id'])->get();
-            foreach ($temp_data as $v) {
-                if ($v['file_path'] > '' AND file_exists('upfire/file' . $v['file_path'])) {
-                    unlink('upfire/file' . $v['file_path']);
-                }
-            }
-            NewsList::destroy($this->input_array['id']);
-        }
-
-        return;
-    }
-
-
-    //上傳圖片檔
-    protected static function uploadFile($file, $id)
-    {
-        $upload_path = env('NEWS_FILE_BATH');
-        $file_name = Str::random(32);
-        $org_name = $file->getClientOriginalName();
-
-        $mime = $file->getMimeType();
-        $ext = '';
-        switch ($mime) {
-            case 'image/png':
-                $ext = 'png';
-                break;
-            case 'image/pjpeg':
-            case 'image/jpg':
-            case 'image/jpeg':
-                $ext = 'jpg';
-                break;
-            case 'image/gif':
-                $ext = 'gif';
-                break;
-            case 'image/x-ms-bmp':
-                $ext = 'bmp';
-                break;
-            default:
-                $_t = explode('.', $org_name);
-                if (count($_t) > 0) {
-                    $ext = $_t[count($_t) - 1];
-                }
-                break;
-        }
-        if (!empty($ext)) {
-            $file_name = "{$file_name}.{$ext}";
-        }
-
-
-        $real_path = public_path() . DIRECTORY_SEPARATOR . $upload_path;
-
-        $year = date('Y');
-        $month = date('m');
-        $day = date('d');
-        if (!file_exists($real_path)) {
-            mkdir($real_path);
-        }
-        $real_path = $real_path . DIRECTORY_SEPARATOR . $year;
-        if (!file_exists($real_path)) {
-            mkdir($real_path);
-        }
-        $real_path = $real_path . DIRECTORY_SEPARATOR . $month;
-        if (!file_exists($real_path)) {
-            mkdir($real_path);
-        }
-        $real_path = $real_path . DIRECTORY_SEPARATOR . $day;
-        if (!file_exists($real_path)) {
-            mkdir($real_path);
-        }
-
-        $destinationPath = $real_path;
-        $file->move($destinationPath, $file_name);
-
-        $file_path = $upload_path . '/' . $year . '/' . $month . '/' . $day . '/' . $file_name;
-
-        $update = NewsList::find($id);
-        $update->file_path = $file_path;
-        $update->file_name = $org_name;
+        $update = new UnitStructure();
+        $update->version = $this->input_array['version'];
+        $update->subject = $this->input_array['subject'];
+        $update->book = $this->input_array['book'];
+        $update->unit_title = $this->input_array['unit_title'];
         $update->save();
+        $this->msg = array(
+            'status' => true,
+            'msg' => '新增成功!',
+        );
 
-        return $update;
+        return $this->msg;
     }
+
+
+    /**
+     * 更新 單元結構資料
+     *
+     */
+    public function update_unit()
+    {
+        if (isset($this->input_array['id']) && isset($this->input_array['unit_title'])) {
+            $update = UnitStructure::find($this->input_array['id']);
+            $update->unit_title = $this->input_array['unit_title'];
+            $update->save();
+            $this->msg = array(
+                'status' => true,
+                'msg' => '更新成功!',
+            );
+        }
+
+        return $this->msg;
+    }
+
+    /**
+     * 移除 單元結構資料
+     *
+     */
+    public function delete_unit()
+    {
+        if (isset($this->input_array['id'])) {
+            UnitStructure::destroy($this->input_array['id']);
+            $this->msg = array(
+                'status' => true,
+                'msg' => '刪除成功!',
+            );
+        }
+
+        return $this->msg;
+    }
+
+    /**
+     * 新增 課程資料
+     *
+     */
+    public function add_course()
+    {
+        $update = new Course();
+        $update->school_year = $this->input_array['school_year'];
+        $update->semester = $this->input_array['semester'];
+        $update->course_title = $this->input_array['course_title'];
+        $update->save();
+        $this->msg = array(
+            'status' => true,
+            'msg' => '新增成功!',
+        );
+
+        return $this->msg;
+    }
+
+
+    /**
+     * 更新 課程資料
+     *
+     */
+    public function update_course()
+    {
+        if (isset($this->input_array['id']) && isset($this->input_array['course_title'])) {
+            $update = Course::find($this->input_array['id']);
+            $update->course_title = $this->input_array['course_title'];
+            $update->save();
+            $this->msg = array(
+                'status' => true,
+                'msg' => '更新成功!',
+            );
+        }
+
+        return $this->msg;
+    }
+
+    /**
+     * 移除 課程資料
+     *
+     */
+    public function delete_course()
+    {
+        if (isset($this->input_array['id'])) {
+            Course::destroy($this->input_array['id']);
+            $this->msg = array(
+                'status' => true,
+                'msg' => '刪除成功!',
+            );
+        }
+
+        return $this->msg;
+    }
+
+
 }
