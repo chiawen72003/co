3e84636316b338d3600f21a4a3970a07b2fe5803
cwc <cwc>
2017-12-06 19:54:01 +0800

    增加 試卷model

---
+++ app/Http/Models/Reel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Models;
+
+class Reel extends BaseModel
+{
+    //試卷資料
+    protected $table = 'reel';
+    protected $primaryKey = 'id';
+    public $timestamps = true;
+}
