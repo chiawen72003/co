b196e567e4b84a0e60ca6e3c67700def282b976e
cwc <cwc>
2017-12-06 22:21:06 +0800

    增加 學校、科系的路徑

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -16,3 +16,12 @@ Route::pattern('uid', '[0-9]+');
 | and give it the controller to call when that URI is requested.
 |
 */
+
+//管理員 學校-科系
+Route::get('/Ma/Subject', ['as' => 'ma.subject', 'uses' => 'AdController@SchoolSubject']);
+Route::get('/Ma/Subject/list', ['as' => 'ma.subject.list', 'uses' => 'AdController@SchoolSubjectList']);
+
+//管理員 管理使用者-匯入學校/代碼
+Route::get('/Ma/School', ['as' => 'ma.school', 'uses' => 'AdController@School']);
+Route::get('/Ma/School/list', ['as' => 'ma.school.list', 'uses' => 'AdController@SchoolList']);
+Route::post('/Ma/School/Add', ['as' => 'ma.school.add', 'uses' => 'AdController@SchoolAdd']);
