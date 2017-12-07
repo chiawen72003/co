0cdbd3b54c06d5209aadd53184c8e4ab571dbee7
chiawen72003 <444>
2017-12-07 09:59:41 +0800

    增加 學校-科系 新增路徑

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -20,6 +20,7 @@ Route::pattern('uid', '[0-9]+');
 //管理員 學校-科系
 Route::get('/Ma/Subject', ['as' => 'ma.subject', 'uses' => 'AdController@SchoolSubject']);
 Route::get('/Ma/Subject/list', ['as' => 'ma.subject.list', 'uses' => 'AdController@SchoolSubjectList']);
+Route::post('/Ma/Subject/Add', ['as' => 'ma.subject.add', 'uses' => 'AdController@SchoolSubjectAdd']);
 
 //管理員 管理使用者-匯入學校/代碼
 Route::get('/Ma/School', ['as' => 'ma.school', 'uses' => 'AdController@School']);
