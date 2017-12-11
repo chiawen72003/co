916272b910f70c2a41392491f63cdce071b2a3ad
cwc <cwc>
2017-12-11 22:36:06 +0800

    增加注意事項

---
+++ app/Http/Models/Precautions.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Models;
+
+class Precautions extends BaseModel
+{
+    //最新消息資料
+    protected $table = 'precautions';
+    protected $primaryKey = 'id';
+    public $timestamps = true;
+}
