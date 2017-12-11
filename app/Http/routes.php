1da9d3cb7416d1a403bda719001ceeb4b7f53e3e
chiawen72003 <444>
2017-12-11 12:07:23 +0800

    新增 兩個路徑

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -45,5 +45,7 @@ Route::post('/Ma/Reel/Add', ['as' => 'ma.reel.add', 'uses' => 'AdController@Reel
 //管理員 試題管理
 Route::get('/Ma/Question', ['as' => 'ma.question', 'uses' => 'AdController@Question']);
 Route::get('/Ma/Question/list', ['as' => 'ma.question.list', 'uses' => 'AdController@QuestionList']);
+Route::get('/Ma/Question/pg/add', ['as' => 'ma.question.pg.add', 'uses' => 'AdController@QuestionPgAdd']);
+Route::get('/Ma/Question/pg/edit/{id}', ['as' => 'ma.question.pg.edit', 'uses' => 'AdController@QuestionPgEdit']);
 Route::post('/Ma/Question/Add', ['as' => 'ma.question.add', 'uses' => 'AdController@QuestionAdd']);
 Route::post('/Ma/Question/Update', ['as' => 'ma.question.update', 'uses' => 'AdController@QuestionUpdate']);
