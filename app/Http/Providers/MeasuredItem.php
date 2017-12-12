8a2290045933f44fd629e4e5c0bedad38eb2a7ba
cwc <cwc>
2017-12-12 22:07:34 +0800

    程式碼調整

--- app/Http/Providers/MeasuredItem.php
+++ app/Http/Providers/MeasuredItem.php
@@ -3,9 +3,8 @@
 namespace App\Http\Providers;
 
 use App\Http\Models\ListUnderTest;
-use App\Http\Models\Course;
-use App\Http\Models\Reel;
-use App\Http\Models\Precautions;
+use App\Http\Models\ReelQuestion;
+use App\Http\Models\Questions;
 use Illuminate\Support\Str;
 use \Input;
 
@@ -38,10 +37,11 @@ class MeasuredItem
     public function getMeasured($user_id)
     {
         $return_data = array();
-        $temp_obj = ListUnderTest::select('id','reel_id')
+        $temp_obj = ListUnderTest::select('reel_id')
+            ->where('user_id',$user_id)
             ->get();
         foreach($temp_obj as $v ){
-            $return_data[] = $v;
+            $return_data[] = route('ur.reel.edit',array($v['reel_id']));
         }
 
         $this->msg = array(
@@ -53,4 +53,38 @@ class MeasuredItem
         return $this->msg;
     }
 
+    /**
+     * 回傳指定試卷內所有試題資料
+     *
+     */
+    public function getReelQuation(){
+        $data = array();
+        $t = ReelQuestion::where('reel_question.reel_id', $this -> input_array['id'])
+            ->leftJoin('questions', 'questions.id', '=', 'reel_question.question_id')
+            ->select(
+                'questions.type',
+                'questions.type_title',
+                'questions.question_title',
+                'questions.dsc',
+                'questions.id'
+            )
+            ->get();
+        foreach ($t as $v){
+            $data[] = array(
+                'type' => $v['type'],
+                'type_title' => json_decode($v['type_title'], true),
+                'question_title' => $v['question_title'],
+                'dsc' => $v['dsc'],
+                'id' => $v['id'],
+            );
+        }
+
+        $this->msg = array(
+            'status' => true,
+            'msg' => '',
+            'data' => $data,
+        );
+
+        return $this->msg;
+    }
 }
