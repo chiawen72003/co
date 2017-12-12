6b551a68ff694feca7b28ca488b75d7749b98660
chiawen72003 <444>
2017-12-12 11:36:01 +0800

    增加受測者-試卷控制項

--- app/Http/Controllers/UrController.php
+++ app/Http/Controllers/UrController.php
@@ -7,6 +7,8 @@ use \Validator;
 use \Session;
 use \DB;
 use \Response;
+use App\Http\Providers\MeasuredItem;
+
 
 class UrController extends Controller
 {
@@ -59,6 +61,73 @@ class UrController extends Controller
      *
      */
     public function UserUpdate()
+    {
+        $data = array();
+        $data['id'] = app('request')->get('id');
+        $data['dsc'] = app('request')->get('dsc');
+        $t_obj = new StructureItem();
+        $t_obj->init($data);
+
+        echo json_encode($t_obj->updatePrecautions());
+    }
+
+    /**
+     * 試卷管理
+     *
+     *
+     */
+    public function Reel()
+    {
+        $data = array();
+
+        return view('user.userdata.index', $data);
+    }
+
+    /**
+     * 試卷管理
+     *
+     *
+     */
+    public function ReelList()
+    {
+        $reel = new MeasuredItem();
+        $id = app('request')->get('id');
+
+        echo json_encode($reel->getMeasured($id));
+    }
+
+
+    /**
+     * 試卷 填寫頁面
+     *
+     *
+     */
+    public function ReelEdit()
+    {
+        $data = array();
+
+        return view('user.userdata.index', $data);
+    }
+
+    /**
+     * 試卷的試題資料
+     *
+     *
+     */
+    public function ReelData()
+    {
+        $question = new StructureItem();
+        $id = app('request')->get('id');
+
+        echo json_encode($question->getPrecautions($id));
+    }
+
+    /**
+     * 新增試卷填寫的資料
+     *
+     *
+     */
+    public function ReelUpdate()
     {
         $data = array();
         $data['id'] = app('request')->get('id');
