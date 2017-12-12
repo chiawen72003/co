507f7db0c6085560f6c9289609dad9f0185500fe
chiawen72003 <444>
2017-12-12 10:10:19 +0800

    增加使用者管理的控制項

--- app/Http/Controllers/UrController.php
+++ app/Http/Controllers/UrController.php
@@ -28,4 +28,44 @@ class UrController extends Controller
         return view('user.index', $data);
     }
 
+    /**
+     * 使用者管理
+     *
+     *
+     */
+    public function User()
+    {
+        $data = array();
+
+        return view('user.userdata.index', $data);
+    }
+
+    /**
+     * 使用者的資料
+     *
+     *
+     */
+    public function UserData()
+    {
+        $question = new StructureItem();
+        $id = app('request')->get('id');
+
+        echo json_encode($question->getPrecautions($id));
+    }
+
+    /**
+     * 更新使用者的資料
+     *
+     *
+     */
+    public function UserUpdate()
+    {
+        $data = array();
+        $data['id'] = app('request')->get('id');
+        $data['dsc'] = app('request')->get('dsc');
+        $t_obj = new StructureItem();
+        $t_obj->init($data);
+
+        echo json_encode($t_obj->updatePrecautions());
+    }
 }
