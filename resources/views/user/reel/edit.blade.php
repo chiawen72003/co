7192a474cffdb55615b670deb0ecdcbe039136a3
chiawen72003 <444>
2017-12-12 16:10:23 +0800

    增加受測內頁

---
+++ resources/views/user/reel/edit.blade.php
@@ -0,0 +1,37 @@
+@extends('user.layout.test')
+@section('content')
+    <div id="page-container">
+        <div id="page-body" class="clearfix">
+            <div class="time-wrap">
+                <div class="time">
+                    剩餘測驗時間: 59 分 57 秒
+                </div>
+            </div>
+            <div class="chapter">
+                <div class="chapter-header i-alert-info">
+                    <span>學校：國立臺中教育大學</span>
+                    <span>身份：學生</span>
+                    <span>學號：ASC106102</span>
+                    <span>姓名：王曉明</span>
+                </div>
+                <div class="chapter-container" id="test_area">
+
+                </div>
+            </div>
+        </div>
+    </div>
+    <script>
+        //控制只能輸入全形的值
+        function onKeyPress_(event)
+        {
+            if(event.which == 13 ){
+                return true;
+            }
+            if (event.which && (event.which > 126 || event.which < 33) ) {
+                event.preventDefault();
+                return true;
+            }
+            return false;
+        }
+    </script>
+@stop
