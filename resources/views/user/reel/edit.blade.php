62b397507fb06f77de6d7b25ac1d346b511dc29c
chiawen72003 <444>
2017-12-13 11:24:47 +0800

    頁面調整

--- resources/views/user/reel/edit.blade.php
+++ resources/views/user/reel/edit.blade.php
@@ -28,21 +28,18 @@
                     敘述性寫作題組評分標準表：
                 </div>
                 <div class="chapter-content">
-                    <div class="chapter-content-article-left" id="obj_1_dsc">
+                    <div class="chapter-content-article-left" id="dsc">
                     </div>
                     <div class="chapter-content-page">
                         <ul class="i-pages">
                             <li><a href="" class="i-link"><i class="ion-ios-arrow-back"></i> 上一頁</a></li>
-                            <li class="current"><a href="">1</a></li>
-                            <li><a href="">2</a></li>
-                            <li><a href="">3</a></li>
                             <li><a href="" class="i-link">下一頁 <i class="ion-ios-arrow-forward"></i></a></li>
                         </ul>
                     </div>
                 </div>
             </div>
             <div class="chapter-content-wrap">
-                <div class="chapter-content-title" id="obj_1_title">
+                <div class="chapter-content-title" id="title">
                 </div>
                 <div class="chapter-content" style="border-top: 0;">
                     <div class="chapter-content-article-right">
@@ -54,18 +51,18 @@
                             <ul class="row">
                                 <li>
                                     已輸入
-                                    <input type="text" class="i-input" style="width: 40px" value="0" disabled id="obj_1_count">
+                                    <input type="text" class="i-input" style="width: 40px" value="0" disabled id="count">
                                     字
                                 </li>
                                 <li class="right pos-right">
-                                    <div class="form-inline">
+                                    <div class="form-inline" style="display: none">
                                         返回第
                                         <input type="text" class="i-input" style="width: 40px">
                                         題
                                     </div>
                                     <div class="form-inline">
-                                        <button class="i-btn">上一題</button>
-                                        <button class="i-btn">下一題</button>
+                                        <button class="i-btn" id="bt_up">上一題</button>
+                                        <button class="i-btn" id="bt_down">下一題</button>
                                     </div>
                                 </li>
                             </ul>
@@ -80,14 +77,11 @@
                     敘述性寫作題組評分標準表：
                 </div>
                 <div class="chapter-content">
-                    <div class="chapter-content-article-left" id="obj_2_dsc">
+                    <div class="chapter-content-article-left" id="dsc">
                     </div>
                     <div class="chapter-content-page">
                         <ul class="i-pages">
                             <li><a href="" class="i-link"><i class="ion-ios-arrow-back"></i> 上一頁</a></li>
-                            <li class="current"><a href="">1</a></li>
-                            <li><a href="">2</a></li>
-                            <li><a href="">3</a></li>
                             <li><a href="" class="i-link">下一頁 <i class="ion-ios-arrow-forward"></i></a></li>
                         </ul>
                     </div>
@@ -95,7 +89,7 @@
             </div>
             <div class="chapter-content-wrap">
                 <div class="chapter-content-title" >
-                    <br id="obj_2_title">
+                    <br id="title">
                     (請點選<a href="">同意</a>或<a href="">不同意</a>，150字為限，5分)
                 </div>
                 <div class="chapter-content" style="border-top: 0;">
@@ -111,18 +105,18 @@
                             <ul class="row">
                                 <li>
                                     已輸入
-                                    <input type="text" class="i-input" style="width: 40px" value="500" disabled id="obj_2_count">
+                                    <input type="text" class="i-input" style="width: 40px" value="500" disabled id="count">
                                     字
                                 </li>
                                 <li class="right pos-right">
-                                    <div class="form-inline">
+                                    <div class="form-inline" style="display: none">
                                         返回第
                                         <input type="text" class="i-input" style="width: 40px">
                                         題
                                     </div>
                                     <div class="form-inline">
-                                        <button class="i-btn">上一題</button>
-                                        <button class="i-btn">下一題</button>
+                                        <button class="i-btn" id="bt_up">上一題</button>
+                                        <button class="i-btn" id="bt_down">下一題</button>
                                     </div>
                                 </li>
                             </ul>
@@ -137,21 +131,18 @@
                     敘述性寫作題組評分標準表：
                 </div>
                 <div class="chapter-content">
-                    <div class="chapter-content-article-left" id="obj_3_dsc">
+                    <div class="chapter-content-article-left" id="dsc">
                     </div>
                     <div class="chapter-content-page">
                         <ul class="i-pages">
                             <li><a href="" class="i-link"><i class="ion-ios-arrow-back"></i> 上一頁</a></li>
-                            <li class="current"><a href="">1</a></li>
-                            <li><a href="">2</a></li>
-                            <li><a href="">3</a></li>
                             <li><a href="" class="i-link">下一頁 <i class="ion-ios-arrow-forward"></i></a></li>
                         </ul>
                     </div>
                 </div>
             </div>
             <div class="chapter-content-wrap">
-                <div class="chapter-content-title" id="obj_3_title">
+                <div class="chapter-content-title" id="title">
                 </div>
                 <div class="chapter-content" style="border-top: 0;">
                     <div class="chapter-content-article-right">
@@ -169,18 +160,18 @@
                             <ul class="row">
                                 <li>
                                     已輸入
-                                    <input type="text" class="i-input" style="width: 40px" value="500" disabled id="obj_3_count">
+                                    <input type="text" class="i-input" style="width: 40px" value="500" disabled id="count">
                                     字
                                 </li>
                                 <li class="right pos-right">
-                                    <div class="form-inline">
+                                    <div class="form-inline" style="display: none">
                                         返回第
                                         <input type="text" class="i-input" style="width: 40px">
                                         題
                                     </div>
                                     <div class="form-inline">
-                                        <button class="i-btn">上一題</button>
-                                        <button class="i-btn">下一題</button>
+                                        <button class="i-btn" id="bt_up">上一題</button>
+                                        <button class="i-btn" id="bt_down">下一題</button>
                                     </div>
                                 </li>
                             </ul>
@@ -195,21 +186,18 @@
                     敘述性寫作題組評分標準表：
                 </div>
                 <div class="chapter-content">
-                    <div class="chapter-content-article-left" id="obj_4_dsc">
+                    <div class="chapter-content-article-left" id="dsc">
                     </div>
                     <div class="chapter-content-page">
                         <ul class="i-pages">
                             <li><a href="" class="i-link"><i class="ion-ios-arrow-back"></i> 上一頁</a></li>
-                            <li class="current"><a href="">1</a></li>
-                            <li><a href="">2</a></li>
-                            <li><a href="">3</a></li>
                             <li><a href="" class="i-link">下一頁 <i class="ion-ios-arrow-forward"></i></a></li>
                         </ul>
                     </div>
                 </div>
             </div>
             <div class="chapter-content-wrap">
-                <div class="chapter-content-title" id="obj_4_title">
+                <div class="chapter-content-title" id="title">
                 </div>
                 <div class="chapter-content" style="border-top: 0;">
                     <div class="chapter-content-article-right">
@@ -230,18 +218,18 @@
                             <ul class="row">
                                 <li>
                                     已輸入
-                                    <input type="text" class="i-input" style="width: 40px" value="500" disabled id="obj_4_count">
+                                    <input type="text" class="i-input" style="width: 40px" value="500" disabled id="count">
                                     字
                                 </li>
                                 <li class="right pos-right">
-                                    <div class="form-inline">
+                                    <div class="form-inline" style="display: none">
                                         返回第
                                         <input type="text" class="i-input" style="width: 40px">
                                         題
                                     </div>
                                     <div class="form-inline">
-                                        <button class="i-btn">上一題</button>
-                                        <button class="i-btn">下一題</button>
+                                        <button class="i-btn" id="bt_up">上一題</button>
+                                        <button class="i-btn" id="bt_down">下一題</button>
                                     </div>
                                 </li>
                             </ul>
@@ -292,70 +280,61 @@
             });
         }
 
+        /**
+         * 1. dsc => 閱讀內容
+         * 2. title => 試題標題
+         * 3. count => 輸入字數加總
+         * 4. textarea => 文字輸入區，綁定文字計算總數的功能
+         * 5. bt_up => 上一頁按鈕
+         * 6. bt_down => 下一頁按鈕
+         */
         function setList() {
             var div_total=0;
-            for(var x=0;x<question_item.length;x++)
+            var max_page = question_item.length;
+            for(var x=0,y=1;x<question_item.length;x++,y++)
             {
+                var t;
                 if(question_item[x]['type'] == 1){
-                    var t = obj_1.clone();
-                    t.find('#obj_1_dsc').html(question_item[x]['dsc']).removeAttr('id');
-                    t.find('#obj_1_title').html(question_item[x]['question_title']).removeAttr('id');
-                    t.find('#obj_1_count').attr('id', 'write_'+x+'_count');
-                    t.find('#textarea').each(function(){
-                        $(this).attr('name', 'write_'+div_total+'_text');
-                        div_total++;
-                    });
-                    t.attr('id','write_'+x);
-                    if(x > 0){
-                     t.hide();
-                    }
-                    test_area.append(t);
+                    t = obj_1.clone();
                 }
                 if(question_item[x]['type'] == 2){
-                    var t = obj_2.clone();
-                    t.find('#obj_2_dsc').html(question_item[x]['dsc']).removeAttr('id');
-                    t.find('#obj_2_title').before(question_item[x]['question_title']).removeAttr('id');
-                    t.find('#obj_2_count').attr('id', 'write_'+x+'_count');
-                    t.find('#textarea').each(function(){
-                        $(this).attr('name', 'write_'+div_total+'_text');
-                        div_total++;
-                    });
-                    t.attr('id','write_'+x);
-                    if(x > 0){
-                        //t.hide();
-                    }
-                    test_area.append(t);
+                    t = obj_2.clone();
                 }
                 if(question_item[x]['type'] == 3){
-                    var t = obj_3.clone();
-                    t.find('#obj_3_dsc').html(question_item[x]['dsc']).removeAttr('id');
-                    t.find('#obj_3_title').before(question_item[x]['question_title']).removeAttr('id');
-                    t.find('#obj_3_count').attr('id', 'write_'+x+'_count');
-                    t.find('#textarea').each(function(){
-                        $(this).attr('name', 'write_'+div_total+'_text');
-                        div_total++;
-                    });
-                    t.attr('id','write_'+x);
-                    if(x > 0){
-                        //t.hide();
-                    }
-                    test_area.append(t);
+                    t = obj_3.clone();
                 }
                 if(question_item[x]['type'] == 4){
-                    var t = obj_4.clone();
-                    t.find('#obj_4_dsc').html(question_item[x]['dsc']).removeAttr('id');
-                    t.find('#obj_4_title').before(question_item[x]['question_title']).removeAttr('id');
-                    t.find('#obj_4_count').attr('id', 'write_'+x+'_count');
-                    t.find('#textarea').each(function(){
-                        $(this).attr('name', 'write_'+div_total+'_text');
-                        div_total++;
-                    });
-                    t.attr('id','write_'+x);
-                    if(x > 0){
-                        //t.hide();
-                    }
-                    test_area.append(t);
+                    t = obj_4.clone();
                 }
+                //試題標題
+                t.find('#title').html(question_item[x]['question_title']).removeAttr('id');
+                //試題內容
+                t.find('#dsc').html(question_item[x]['dsc']).removeAttr('id');
+                //使用者在每一個試題內打字的總數量
+                t.find('#count').attr('id', 'write_'+x+'_count');
+                //試題輸入區
+                t.find('#textarea').each(function(){
+                    $(this).attr('name', 'write_'+div_total+'_text');
+                    div_total++;
+                });
+                //上一試題按鈕
+                if(x == 0){
+                    t.find('#bt_up').hide().removeAttr('id');
+                }else{
+                    var t_num = x - 1;
+                    t.find('#bt_up').attr('onclick', 'page_change("'+ t_num +'")');
+                }
+                //下一試題按鈕
+                if(y == max_page){
+                    t.find('#bt_down').html('送出').attr('onclick', 'send_data()').removeAttr('id');
+                }else{
+                    t.find('#bt_down').attr('onclick', 'page_change("'+ y +'")').removeAttr('id');
+                }
+                t.attr('id','write_'+x);
+                if(x > 0){
+                    t.hide();
+                }
+                test_area.append(t);
             }
             //文字輸入區綁定計算字數的功能
             for(var x=0;x<div_total;x++){
@@ -391,5 +370,64 @@
                 $('#write_'+x+'_count').val(total);
             }
         }
+
+        //試題切換
+        function page_change(num)
+        {
+            for(var x=0;x<question_item.length;x++)
+            {
+                if(num == x){
+                    $('write_'+x).show();
+                }else{
+                    $('write_'+x).hide();
+                }
+            }
+        }
+
+        //上傳資料
+        var is_send = false;
+        var send_data = [];
+        function send_data()
+        {
+            if(!is_send){
+                for(var x=0;x<question_item.length;x++)
+                {
+                    var t = $('#write_'+x);
+                    var temp_data = [];
+                    var agree = 0;
+                    t.find('#textarea').each(function(){
+                        temp_data.push({
+                            'dsc':$(this).text()
+                        });
+                    });
+                    send_data.push({
+                        'id':question_item[x]['id'],
+                        'type':question_item[x]['type'],
+                        'dsc':temp_data,
+                        'agree':agree,
+                    });
+                }
+
+                $.ajax({
+                    url: "[! route('ur.reel.add') !]",
+                    type:'POST',
+                    dataType: "json",
+                    data: {
+                        _token: '[! csrf_token() !]',
+                        add_data:send_data,
+                    },
+                    error: function(xhr) {
+                        //alert('Ajax request 發生錯誤');
+                    },
+                    success: function(response) {
+                        if(response['status'] == true){
+                            alert(response['msg']);
+                            location.replace("[! route('ur.reel') !]");
+                        }
+                    }
+                });
+            }
+        }
+        
     </script>
 @stop
