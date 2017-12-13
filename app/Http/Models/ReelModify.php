97030c7ce4d5568bc4789c9095b15003ed306476
cwc <cwc>
2017-12-13 21:13:15 +0800

    增加model

---
+++ app/Http/Models/ReelModify.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Models;
+
+class ReelModify extends BaseModel
+{
+    //試卷-評閱者資料
+    protected $table = 'reel_modify';
+    protected $primaryKey = 'id';
+    public $timestamps = true;
+}
