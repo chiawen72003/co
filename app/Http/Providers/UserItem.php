84e3df736257698fcdb7dba57525c88d743bcd52
cwc <cwc>
2017-12-15 22:37:33 +0800

    新增物件

---
+++ app/Http/Providers/UserItem.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Providers;
+
+use App\Http\Models\Revised;
+use App\Http\Models\ReelQuestion;
+use App\Http\Models\ReelModify;
+use Illuminate\Support\Str;
+use \Input;
+use Prophecy\Prophecy\Revealer;
+
+/**
+ * Class UserItem
+ * 結構物件：管理員、評閱者、受測者資料
+ *
+ * @package App\Http\Providers處理
+ */
+class UserItem
+{
+    private $input_array = array();
+
+    private $msg = array(
+        'status' => false,
+        'msg' => '',
+    );
+
+    public function init($input_data = array())
+    {
+        foreach ($input_data as $k => $v) {
+            $this->input_array[$k] = $v;
+        }
+    }
+
+    /**
+     * 取得 評閱者資料
+     *
+     */
+    public function getReviewer()
+    {
+        $return_data = array();
+        $temp_obj = Revised::select('login_name','login_pw','name')
+            ->where('id',1)
+            ->get();
+        foreach($temp_obj as $v ){
+            $return_data = array(
+                'login_name' => $v['login_name'],
+                'login_pw' => $v['login_pw'],
+                'name' => $v['name'],
+            );
+        }
+
+        $this->msg = array(
+            'status' => true,
+            'msg' => '',
+            'data' => $return_data,
+        );
+
+        return $this->msg;
+    }
+
+    /**
+     * 更新 評閱者密碼資料
+     *
+     */
+    public function setReviewerPw()
+    {
+        if (isset($this->input_array['new_pw'])) {
+            $update = Revised::find($this->input_array['id']);
+            $update->login_pw = $this->input_array['new_pw'];
+            $update->save();
+            $this->msg = array(
+                'status' => true,
+                'msg' => '更新成功!',
+            );
+        }
+
+        return $this->msg;
+    }
+}
