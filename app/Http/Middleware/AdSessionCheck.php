8c3fb7cd30c6252ac8dea20a2dcbeda11bbec5f0
cwc <cwc>
2017-12-19 22:15:14 +0800

    程式碼調整

--- app/Http/Middleware/AdSessionCheck.php
+++ app/Http/Middleware/AdSessionCheck.php
@@ -6,16 +6,13 @@ use Closure;
 
 class AdSessionCheck
 {
-    private $ad_level = array(
-        '91','92'
-    );
     public function handle($request, Closure $next, $guard = null)
     {
-        if ( !$request->session()->has('user_data.access_level') OR
-             !in_array($request->session()->get('user_data.access_level'),$this->ad_level)
+        if ( !$request->session()->has('user_type') OR
+             $request->session()->get('user_type') != 'Admin'
             )
         {
-            return redirect()->route('ad.logout');
+            return redirect()->route('member.login');
         }
 
         return $next($request);
