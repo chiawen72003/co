7fdd747524dc9a62320ebf4d7a15ab5bbb0bb36b
chiawen72003 <444>
2017-12-19 12:34:13 +0800

    程式碼調整

--- app/Http/Providers/ModifyItem.php
+++ app/Http/Providers/ModifyItem.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Providers;
 
+use App\Http\Models\ListUnderTest;
 use App\Http\Models\ReelModify;
 use Illuminate\Support\Str;
 use \Input;
@@ -36,7 +37,7 @@ class ModifyItem
     public function getReelList()
     {
         $return_data = array();
-        $temp_obj = ReelModify::where('reel_modify.user_id',$this->input_array['id'])
+        $temp_obj = ReelModify::where('reel_modify.user_id', $this->input_array['id'])
             ->leftJoin('reel', 'reel_modify.reel_id', '=', 'reel.id')
             ->select(
                 'reel_modify.view_num',
@@ -45,7 +46,7 @@ class ModifyItem
                 'reel.reel_title'
             )
             ->get();
-        foreach($temp_obj as $v ){
+        foreach ($temp_obj as $v) {
             $return_data[] = array(
                 'path' => route('rv.scroll.reel.view', array($v['reel_id'])),
                 'reel_title' => $v['reel_title'],
@@ -63,4 +64,32 @@ class ModifyItem
         return $this->msg;
     }
 
+    /**
+     * 新增 評閱者-試卷資料
+     *
+     * 備註：新增時就先指派現有的試卷給此評閱者
+     */
+    public function addReel()
+    {
+        $update = new ReelModify();
+        $update->reel_id = $this->input_array['reel_id'];
+        $update->user_id = $this->input_array['user_id'];
+        $update->need_num = $this->input_array['need_num'];
+        $update->view_num = 0;
+        $update->save();
+        //處理指派
+        ListUnderTest::where('modify_id', '0')
+            ->where('reel_id', $this->input_array['reel_id'])
+            ->where('has_test', '1')
+            ->where('has_review', '0')
+            ->limit($this->input_array['need_num'])
+            ->update(['modify_id'=>$this->input_array['user_id']]);
+
+        $this->msg = array(
+            'status' => true,
+            'msg' => '新增成功!',
+        );
+
+        return $this->msg;
+    }
 }
