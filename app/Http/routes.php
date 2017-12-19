eb5aa2729ff0566cc5a8953bf008a3d6678ad476
cwc <cwc>
2017-12-19 22:14:23 +0800

    管理員增加中介層

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -21,65 +21,69 @@ Route::pattern('uid', '[0-9]+');
 Route::get('/', ['as' => 'member.login', 'uses' => 'MemController@Login']);
 Route::get('/Logout', ['as' => 'member.logout', 'uses' => 'MemController@Logout']);
 Route::post('/Login/Chk', ['as' => 'member.login.chk', 'uses' => 'MemController@LoginChk']);
+Route::get('/Login/School/list', ['as' => 'member.school.list', 'uses' => 'MemController@SchoolList']);
 
-//管理員 最新消息
-Route::get('/Ma/News', ['as' => 'ma.news', 'uses' => 'AdController@News']);
-Route::get('/Ma/News/list', ['as' => 'ma.news.list', 'uses' => 'AdController@NewsList']);
-Route::post('/Ma/News/Add', ['as' => 'ma.news.add', 'uses' => 'AdController@NewsAdd']);
-Route::post('/Ma/News/Update', ['as' => 'ma.news.update', 'uses' => 'AdController@NewsUpdate']);
+//管理員端
+Route::group(['middleware' => 'AdSessionCheck'], function () {
+    //管理員 最新消息
+    Route::get('/Ma/News', ['middleware' => 'IsLoginCheck','as' => 'ma.news', 'uses' => 'AdController@News']);
+    Route::get('/Ma/News/list', ['as' => 'ma.news.list', 'uses' => 'AdController@NewsList']);
+    Route::post('/Ma/News/Add', ['as' => 'ma.news.add', 'uses' => 'AdController@NewsAdd']);
+    Route::post('/Ma/News/Update', ['as' => 'ma.news.update', 'uses' => 'AdController@NewsUpdate']);
 
-//管理員 學校-科系
-Route::get('/Ma/Subject', ['as' => 'ma.subject', 'uses' => 'AdController@SchoolSubject']);
-Route::get('/Ma/Subject/list', ['as' => 'ma.subject.list', 'uses' => 'AdController@SchoolSubjectList']);
-Route::post('/Ma/Subject/Add', ['as' => 'ma.subject.add', 'uses' => 'AdController@SchoolSubjectAdd']);
+    //管理員 學校-科系
+    Route::get('/Ma/Subject', ['as' => 'ma.subject', 'uses' => 'AdController@SchoolSubject']);
+    Route::get('/Ma/Subject/list', ['as' => 'ma.subject.list', 'uses' => 'AdController@SchoolSubjectList']);
+    Route::post('/Ma/Subject/Add', ['as' => 'ma.subject.add', 'uses' => 'AdController@SchoolSubjectAdd']);
 
-//管理員 管理使用者-匯入學校/代碼
-Route::get('/Ma/School', ['as' => 'ma.school', 'uses' => 'AdController@School']);
-Route::get('/Ma/School/list', ['as' => 'ma.school.list', 'uses' => 'AdController@SchoolList']);
-Route::post('/Ma/School/Add', ['as' => 'ma.school.add', 'uses' => 'AdController@SchoolAdd']);
+    //管理員 管理使用者-匯入學校/代碼
+    Route::get('/Ma/School', ['as' => 'ma.school', 'uses' => 'AdController@School']);
+    Route::get('/Ma/School/list', ['as' => 'ma.school.list', 'uses' => 'AdController@SchoolList']);
+    Route::post('/Ma/School/Add', ['as' => 'ma.school.add', 'uses' => 'AdController@SchoolAdd']);
 
-//管理員 課程設定
-Route::get('/Ma/Course', ['as' => 'ma.course', 'uses' => 'AdController@Course']);
-Route::get('/Ma/Course/list', ['as' => 'ma.course.list', 'uses' => 'AdController@CourseList']);
-Route::post('/Ma/Course/Add', ['as' => 'ma.course.add', 'uses' => 'AdController@CourseAdd']);
+    //管理員 課程設定
+    Route::get('/Ma/Course', ['as' => 'ma.course', 'uses' => 'AdController@Course']);
+    Route::get('/Ma/Course/list', ['as' => 'ma.course.list', 'uses' => 'AdController@CourseList']);
+    Route::post('/Ma/Course/Add', ['as' => 'ma.course.add', 'uses' => 'AdController@CourseAdd']);
 
-//管理員 單元結構
-Route::get('/Ma/Unit', ['as' => 'ma.unit', 'uses' => 'AdController@Unit']);
-Route::get('/Ma/Unit/list', ['as' => 'ma.unit.list', 'uses' => 'AdController@UnitList']);
-Route::post('/Ma/Unit/Add', ['as' => 'ma.unit.add', 'uses' => 'AdController@UnitAdd']);
+    //管理員 單元結構
+    Route::get('/Ma/Unit', ['as' => 'ma.unit', 'uses' => 'AdController@Unit']);
+    Route::get('/Ma/Unit/list', ['as' => 'ma.unit.list', 'uses' => 'AdController@UnitList']);
+    Route::post('/Ma/Unit/Add', ['as' => 'ma.unit.add', 'uses' => 'AdController@UnitAdd']);
 
-//管理員 試卷設定
-Route::get('/Ma/Reel', ['as' => 'ma.reel', 'uses' => 'AdController@Reel']);
-Route::get('/Ma/Reel/list', ['as' => 'ma.reel.list', 'uses' => 'AdController@ReelList']);
-Route::post('/Ma/Reel/Add', ['as' => 'ma.reel.add', 'uses' => 'AdController@ReelAdd']);
+    //管理員 試卷設定
+    Route::get('/Ma/Reel', ['as' => 'ma.reel', 'uses' => 'AdController@Reel']);
+    Route::get('/Ma/Reel/list', ['as' => 'ma.reel.list', 'uses' => 'AdController@ReelList']);
+    Route::post('/Ma/Reel/Add', ['as' => 'ma.reel.add', 'uses' => 'AdController@ReelAdd']);
 
-//管理員 試題管理
-Route::get('/Ma/Question', ['as' => 'ma.question', 'uses' => 'AdController@Question']);
-Route::get('/Ma/Question/list', ['as' => 'ma.question.list', 'uses' => 'AdController@QuestionList']);
-Route::get('/Ma/Question/data', ['as' => 'ma.question.data', 'uses' => 'AdController@QuestionData']);
-Route::get('/Ma/Question/pg/add', ['as' => 'ma.question.pg.add', 'uses' => 'AdController@QuestionPgAdd']);
-Route::get('/Ma/Question/pg/edit', ['as' => 'ma.question.pg.edit', 'uses' => 'AdController@QuestionPgEdit']);
-Route::post('/Ma/Question/Add', ['as' => 'ma.question.add', 'uses' => 'AdController@QuestionAdd']);
-Route::post('/Ma/Question/Update', ['as' => 'ma.question.update', 'uses' => 'AdController@QuestionUpdate']);
+    //管理員 試題管理
+    Route::get('/Ma/Question', ['as' => 'ma.question', 'uses' => 'AdController@Question']);
+    Route::get('/Ma/Question/list', ['as' => 'ma.question.list', 'uses' => 'AdController@QuestionList']);
+    Route::get('/Ma/Question/data', ['as' => 'ma.question.data', 'uses' => 'AdController@QuestionData']);
+    Route::get('/Ma/Question/pg/add', ['as' => 'ma.question.pg.add', 'uses' => 'AdController@QuestionPgAdd']);
+    Route::get('/Ma/Question/pg/edit', ['as' => 'ma.question.pg.edit', 'uses' => 'AdController@QuestionPgEdit']);
+    Route::post('/Ma/Question/Add', ['as' => 'ma.question.add', 'uses' => 'AdController@QuestionAdd']);
+    Route::post('/Ma/Question/Update', ['as' => 'ma.question.update', 'uses' => 'AdController@QuestionUpdate']);
 
-//管理員 注意事項
-Route::get('/Ma/Precautions', ['as' => 'ma.precautions', 'uses' => 'AdController@Precautions']);
-Route::get('/Ma/Precautions/Data', ['as' => 'ma.precautions.data', 'uses' => 'AdController@PrecautionsData']);
-Route::post('/Ma/Precautions/Update', ['as' => 'ma.precautions.update', 'uses' => 'AdController@PrecautionsUpdate']);
+    //管理員 注意事項
+    Route::get('/Ma/Precautions', ['as' => 'ma.precautions', 'uses' => 'AdController@Precautions']);
+    Route::get('/Ma/Precautions/Data', ['as' => 'ma.precautions.data', 'uses' => 'AdController@PrecautionsData']);
+    Route::post('/Ma/Precautions/Update', ['as' => 'ma.precautions.update', 'uses' => 'AdController@PrecautionsUpdate']);
 
-//管理員 評閱者資料管理
-Route::get('/Ma/revised', ['as' => 'ma.revised', 'uses' => 'AdController@Revised']);
-Route::get('/Ma/revised/List', ['as' => 'ma.revised.list', 'uses' => 'AdController@RevisedList']);
-Route::get('/Ma/revised/Data', ['as' => 'ma.revised.data', 'uses' => 'AdController@RevisedData']);
-Route::get('/Ma/revised/Add/Pg', ['as' => 'ma.revised.add.pg', 'uses' => 'AdController@RevisedAddPg']);
-Route::get('/Ma/revised/Edit/Pg', ['as' => 'ma.revised.edit.pg', 'uses' => 'AdController@RevisedEditPg']);
-Route::post('/Ma/revised/Add', ['as' => 'ma.revised.add', 'uses' => 'AdController@RevisedAdd']);
-Route::post('/Ma/revised/Update', ['as' => 'ma.revised.update', 'uses' => 'AdController@RevisedUpdate']);
+    //管理員 評閱者資料管理
+    Route::get('/Ma/revised', ['as' => 'ma.revised', 'uses' => 'AdController@Revised']);
+    Route::get('/Ma/revised/List', ['as' => 'ma.revised.list', 'uses' => 'AdController@RevisedList']);
+    Route::get('/Ma/revised/Data', ['as' => 'ma.revised.data', 'uses' => 'AdController@RevisedData']);
+    Route::get('/Ma/revised/Add/Pg', ['as' => 'ma.revised.add.pg', 'uses' => 'AdController@RevisedAddPg']);
+    Route::get('/Ma/revised/Edit/Pg', ['as' => 'ma.revised.edit.pg', 'uses' => 'AdController@RevisedEditPg']);
+    Route::post('/Ma/revised/Add', ['as' => 'ma.revised.add', 'uses' => 'AdController@RevisedAdd']);
+    Route::post('/Ma/revised/Update', ['as' => 'ma.revised.update', 'uses' => 'AdController@RevisedUpdate']);
 
-//管理員 評閱者-試卷資料管理
-Route::get('/Ma/revised/Reel', ['as' => 'ma.revised.reel', 'uses' => 'AdController@RevisedReel']);
-Route::get('/Ma/revised/Reel/List', ['as' => 'ma.revised.reel.list', 'uses' => 'AdController@RevisedReelList']);
-Route::post('/Ma/revised/Reel/Add', ['as' => 'ma.revised.reel.add', 'uses' => 'AdController@RevisedReelAdd']);
+    //管理員 評閱者-試卷資料管理
+    Route::get('/Ma/revised/Reel', ['as' => 'ma.revised.reel', 'uses' => 'AdController@RevisedReel']);
+    Route::get('/Ma/revised/Reel/List', ['as' => 'ma.revised.reel.list', 'uses' => 'AdController@RevisedReelList']);
+    Route::post('/Ma/revised/Reel/Add', ['as' => 'ma.revised.reel.add', 'uses' => 'AdController@RevisedReelAdd']);
+});
 
 //受測者 首頁
 Route::get('/Ur', ['as' => 'ur.index', 'uses' => 'UrController@Index']);
