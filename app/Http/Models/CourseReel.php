185f6984db6017c9c0ac837af414f7ac1a3391df
cwc <cwc>
2017-12-20 20:06:00 +0800

    新增model

---
+++ app/Http/Models/CourseReel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Models;
+
+class CourseReel extends BaseModel
+{
+    //課程-試卷資料
+    protected $table = 'course_reel';
+    protected $primaryKey = 'id';
+    public $timestamps = true;
+}
