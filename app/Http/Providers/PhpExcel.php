7353dd526c5ce5c9776692b801836d76d14c7046
cwc <cwc>
2018-01-21 14:31:47 +0800

    增加excel處理套件

---
+++ app/Http/Providers/PhpExcel.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Providers;
+
+use PhpOffice\PhpSpreadsheet\IOFactory;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Excel5;
+
+class PhpExcel
+{
+    private $excel_data = array();
+    private $spreadsheet_obj = null;
+    private $writer_obj = null;
+    private $file_name = null;
+    private $input_data = array(
+        'import_file_name' => null,
+    );
+
+    public function __construct()
+    {
+        $this->spreadsheet_obj = new Spreadsheet();
+        $this->file_name = date("Y-m-d") . ".xls";
+    }
+
+    public function init($data = array())
+    {
+        foreach ($data as $k => $v) {
+            $this->input_data[$k] = $v;
+        }
+    }
+
+    /**
+     * 設定輸出資料
+     *
+     * @param $get_data 輸出的資料
+     */
+    public function set_excel_data($get_data)
+    {
+        $this->excel_data = $get_data;
+    }
+
+    /**
+     * 設定存檔名稱
+     *
+     * @param $get_data 輸出的資料
+     */
+    public function set_file_name($get_data)
+    {
+        $this->file_name = $get_data;
+    }
+
+    /**
+     * 匯入 班級內學生的資料
+     */
+    public function import_student_data()
+    {
+        $return_data = array();
+
+        if ($this->input_data['import_file_name']) {
+            $spreadsheet = \PhpOffice\PhpSpreadsheet\IOFactory::load($this->input_data['import_file_name']);
+            $worksheet = $spreadsheet->getActiveSheet();
+            $highestRow = $worksheet->getHighestRow(); // e.g. 10
+            //如果對應欄位沒有值，會用null代替
+            $dataArray = $worksheet->rangeToArray(
+                'B2:k' . $highestRow,
+                NULL,        // Value that should be returned for empty cells
+                TRUE,        // Should formulas be calculated (the equivalent of getCalculatedValue() for each cell)
+                TRUE,        // Should values be formatted (the equivalent of getFormattedValue() for each cell)
+                TRUE         // Should the array be indexed by cell row and cell column
+            );
+
+            return $dataArray;
+        }
+    }
+
+}
