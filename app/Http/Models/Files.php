81c304f7ee67b9b81ec0fa5a0011571b9dac7088
cwc <cwc>
2018-02-05 20:53:32 +0800

    增加檔案

---
+++ app/Http/Models/Files.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Models;
+
+class Files extends BaseModel
+{
+    //縣市資料
+    protected $table = 'files';
+    protected $primaryKey = 'id';
+    public $timestamps = true;
+}
