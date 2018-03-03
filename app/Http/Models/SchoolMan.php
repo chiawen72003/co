8e08ca91b4edbc2d60d0814d3adb6031d0038d33
cwc <cwc>
2018-03-03 10:49:05 +0800

    增加model

---
+++ app/Http/Models/SchoolMan.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Models;
+
+class SchoolMan extends BaseModel
+{
+    //學校資料
+    protected $table = 'school_man';
+    protected $primaryKey = 'id';
+    public $timestamps = true;
+}
